448340efff79c071c0c6259b8653c53de2611de4
JPPalacios <56488109+JPPalacios@users.noreply.github.com>
2020-03-18 22:21:45 -0400

    update:10:21

--- LAB 9/DRAFT CODE/EGR 226 - LAB 9 - PART II.c
+++ LAB 9/DRAFT CODE/EGR 226 - LAB 9 - PART II.c
@@ -7,20 +7,24 @@
 
 #include "msp.h"
 
-void SysTick_Init(void);                // prototype: SysTick Initialization Function
-void SysTick_Ms_delay(uint16_t msec);   // prototype: SysTick delay in milliseconds
+void SysTick_Init_Interupt(void);       // prototype: SysTick initialization with Interupts enabled function
+void SysTick_Handler(void);             // prototype: SysTick Handler function
+//void SysTick_Ms_delay(uint16_t msec);   // prototype: SysTick delay in milliseconds
 
 void Segment_pinsetup(void);            // prototype: 7 Segment LCD display pin initialization
 void Segment_display(void);             // prototype: 7 Segment LCD Display function
 
+volatile uint32_t count;
+
 const unsigned char digitPattern[] =
-    {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+    {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x80};
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-	 SysTick_Init();     // initialize: all functions
+	 SysTick_Init_Interupt();     // initialize: all functions
+	 __enable_irq();              // enable global interrupts
 	 Segment_pinsetup();
 
 	 while(1){
@@ -30,9 +34,9 @@ void main(void)
 }
 
 void Segment_display(void){
-    P4->OUT = digitPattern[8];
+    P4->OUT = digitPattern[count];
     P5->OUT |= 0x01;
-    SysTick_Ms_delay(10);
+    //SysTick_Ms_delay(10);
 }
 
 void Segment_pinsetup(void){
@@ -49,17 +53,22 @@ void Segment_pinsetup(void){
 /* SYSTICK FUNCTIONS*/
 
 /*This function initializes the SysTick Timer*/
-void SysTick_Init(void){
+void SysTick_Init_Interupt(void){
 SysTick -> CTRL = 0;
-SysTick -> LOAD = 0x00FFFFFF;
+SysTick -> LOAD = 0x002DC6C0;
 SysTick -> VAL = 0;
-SysTick -> CTRL = 0x00000005;
+SysTick -> CTRL = 0x00000007;
 }
 
-/*This function is used as a delay in milliseconds using SysTick.
-* modified */
+void SysTick_Handler(void){
+    count++;
+}
+
+/*This function is used as a delay in milliseconds using SysTick. modified */
+/*
 void SysTick_Ms_delay(uint16_t msec){
 SysTick->LOAD = ((msec * 3000)- 1);
 SysTick->VAL = 0;
 while((SysTick->CTRL & 0x00010000) == 0);
 }
+*/
